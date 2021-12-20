0c46ebc91381311df91394002984b8ac1b8fc160
Orrmey <orrmey1995@gmail.com>
2021-12-20 22:08:11 +0200

    created authentication forms

---
+++ web/accounts/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import (
+    authenticate,
+    get_user_model,
+    login,
+    logout
+)
+from .forms import UserLoginForm, UserRegisterForm
+
+
+def login_view(request, *args, **kwargs):
+    next = request.GET.get('next')
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect('/')
+    
+    context = {
+        'form': form,
+    }
+    return render(request, "login.html", context)
+
+def register_view(request, *args, **kwargs):
+    next = request.GET.get('next')
+    form = UserRegisterForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        password = form.cleaned_data.get('password')
+        user.set_password(password)
+        user.save()
+        new_user = authenticate(username=user.username, password=password)
+        
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect('/')
+    
+    context = {
+        'form': form,
+    }
+    return render(request, "signup.html", context)
+
+def logout_view(request, *args, **kwargs):
+    logout(request)
+    return redirect('/')
+
+@login_required
+def home(request, *args, **kwargs):
+    return render(request, "home.html",{})
